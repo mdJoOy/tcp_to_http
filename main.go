8310a558a3a1bcfdd2737b25bda0af4b49c58613
Joy <mdjoynalabedin2003@gmail.com>
2026-02-03 12:04:31 +1100

    making it a reuseable func and implementing concurency and channel

--- main.go
+++ main.go
@@ -3,32 +3,46 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+	}()
+	return out
+}
 
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("error", err)
 	}
-	defer f.Close()
 
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:n]
-
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Println(str)
-			str = ""
-		}
-		str += string(data)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("%s\n", line)
 	}
 }
