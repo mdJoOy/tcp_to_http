a4930837693a777fed174253231af4d21242451c
Joy <mdjoynalabedin2003@gmail.com>
2026-02-21 06:17:23 +1100

    adding case insensitivity and token varification for header name

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,16 +3,39 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
-type Headers map[string]string
+type Headers struct {
+	headers map[string]string
+}
 
 var crlf = []byte("\r\n")
 
-func NewHeaders() Headers {
-	return map[string]string{}
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
 }
+func isToken(str []byte) bool {
+	for _, ch := range str {
+		found := false
+		if ch >= 'A' && ch <= 'Z' ||
+			ch >= 'a' && ch <= 'z' ||
+			ch >= '0' && ch <= '9' {
+			found = true
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+		if !found {
+			return false
+		}
 
+	}
+	return true
+}
 func parseHeader(filedline []byte) (string, string, error) {
 	parts := bytes.SplitN(filedline, []byte(":"), 2)
 	if len(parts) != 2 {
@@ -27,7 +50,15 @@ func parseHeader(filedline []byte) (string, string, error) {
 	return string(name), string(value), nil
 }
 
-func (h Headers) Parse(data []byte) (int, bool, error) {
+func (h *Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+func (h *Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
+
+}
+
+func (h *Headers) Parse(data []byte) (int, bool, error) {
 	read := 0
 	done := false
 
@@ -47,9 +78,12 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 		if err != nil {
 			return 0, false, err
 		}
+		if !isToken([]byte(name)) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
 
 		read += idx + len(crlf)
-		h[name] = value
+		h.Set(name, value)
 	}
 	return read, done, nil
 }
