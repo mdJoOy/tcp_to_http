fe82bae13e897283cf09290b2f3f4b5eb505c178
Joy <mdjoynalabedin2003@gmail.com>
2026-02-21 11:54:36 +1100

    updating set so that it can have multiple values for a single header

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -25,4 +25,11 @@ func TestHeaderParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	assert.Equal(t, "localhost:42069,localhost:42069", headers.Get("HOST"))
+	require.NoError(t, err)
+	assert.False(t, done)
 }
